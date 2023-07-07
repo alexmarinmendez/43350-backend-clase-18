2168c534d3d72312a54ad37f6529b8d4a8ac07f8
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-07-06 20:15:46 -0500

    feat: sessions en memoria

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import express from 'express'
-import cookieParser from 'cookie-parser'
+import session from 'express-session'
 
 const app = express()
-app.use(cookieParser('victoriasecret'))
+app.use(session({
+    secret: 'victoriasecret',
+    resave: true,
+    saveUninitialized: true
+}))
 
 app.get('/', (req, res) => res.json({ status: 'success', message: 'Que la fueza te acompañe!' }))
 app.get('/user/profile', (req, res) => {
@@ -12,16 +16,21 @@ app.get('/user/profile', (req, res) => {
         language: 'es',
         location: 'pe'
     }
-    res.cookie('preference', JSON.stringify(user), {signed: true}).json({ status: 'success', message: 'Cookie creada!' })
+    // res.cookie('preference', JSON.stringify(user), {signed: true}).json({ status: 'success', message: 'Cookie creada!' })
+    req.session.user = user
+    res.json({ status: 'success', message: 'Session creada!' })
 })
 
 app.get('/user/getpreference', (req, res) => {
-    const preference = JSON.parse(req.signedCookies['preference'])
-    res.send(preference.location)
+    res.send(req.session.user.username)
 })
 
 app.get('/user/deletepreference', (req, res) => {
-    res.clearCookie('preference').json({ status: 'success', message: 'Cookie deleteada!' })
+    // res.clearCookie('preference').json({ status: 'success', message: 'Cookie deleteada!' })
+    req.session.destroy(err => {
+        if (err) return res.json({ status: 'error', message: 'Ocurrio un error' })
+        return res.json({ status: 'success', message: 'Cookie deleteada!' })
+    })
 })
 
 app.listen(8080, () => console.log('Server Up'))
